44481d8faf22df497bde3abf9a95e9ab07c49b25
andreaditya <andreaditya21@gmail.com>
2020-10-02 00:41:54 +0700

    tambah user

---
+++ app.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const PORT = 3000
+const cors = require('cors')
+
+const userRoutes = require('./routes/users')
+
+const errorHandler = require('./middlewares/errorHandler')
+
+app.use(express.urlencoded ({extended: false}))
+app.use(express.json())
+app.use(cors())
+
+app.use('/', userRoutes)
+app.use(errorHandler)
+
+app.listen(PORT, () => {
+    console.log(`running app on port ${PORT}`)
+})
