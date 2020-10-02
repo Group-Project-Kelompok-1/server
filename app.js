067b5cf877504afc9e35c2018719f09ef3f45450
dirabbieto <dirabbieto@gmail.com>
2020-10-02 12:38:08 +0700

    google-sign-in

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const PORT = 3000
 const routes = require('./routes/index.js')
 
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
+app.use(cors())
 
 app.use('/', routes)
 
-app.listen(PORT,()=>{
+app.listen(PORT,() => {
   console.log(`listen on ${PORT}`)
 })
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const UserController = require('../controllers/user-controller.js')
 
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
+router.post('/googleSignIn', UserController.googleSignIn)
 
 
 module.exports= router
